f7c7f6701a9fe5f0dd81d42eff54f4cc1023fce4
SEGVeenstra <seg.veenstra@gmail.com>
2022-09-17 21:42:14 +0200

    Use FutureOr to allow async handlers

--- etos/lib/src/etos.dart
+++ etos/lib/src/etos.dart
@@ -1,16 +1,20 @@
+import 'dart:async';
+
 import 'package:logging/logging.dart';
 import 'package:rxdart/subjects.dart';
 
 final _logger = Logger('Etos');
 
-typedef EtosHandler<Tstate extends Object> = Tstate Function(
+typedef EtosHandler<Tstate extends Object> = FutureOr<Tstate> Function(
     Object event, Tstate state);
 
 class Etos<Tstate extends Object> {
   final _eventHandlers = <Type, EtosHandler<Tstate>>{};
   final _state = BehaviorSubject<Tstate>();
+  final _events = StreamController<Object>();
 
   Stream<Tstate> get state => _state.stream;
+  Stream<Object> get events => _events.stream;
 
   Etos({required Tstate state}) {
     _logger.info('Creeated with initial state:\n$state');
@@ -28,8 +32,9 @@ class Etos<Tstate extends Object> {
     _logger.info('Eventhandler added for $Tevent');
   }
 
-  void dispatch(Object event) {
+  void dispatch(Object event) async {
     _logger.info('dispatched event\n$event');
+    _events.add(event);
 
     final handler = _eventHandlers[event.runtimeType];
 
@@ -38,7 +43,7 @@ class Etos<Tstate extends Object> {
     }
 
     final currentState = _state.value;
-    final newState = handler.call(event, currentState);
+    final newState = await handler.call(event, currentState);
 
     _state.add(newState);
     _logger.info('state updated\n$newState');
